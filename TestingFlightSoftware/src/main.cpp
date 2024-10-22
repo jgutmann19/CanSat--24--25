e3c85f72684a0595b0cc919ee91d2009df2e1b5f
Eduardo <edward.steinmann@outlook.com>
2024-10-22 19:16:00 -0400

    Added test project for platformIO, built it, and Serial was working.

---
+++ TestingFlightSoftware/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+#define SERIAL_BITS_PER_SECOND 115200
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(SERIAL_BITS_PER_SECOND);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
