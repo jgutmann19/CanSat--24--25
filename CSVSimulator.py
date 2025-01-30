76cccf1a4581e6ca3ef8c9847e94b4b150dcdaea
Joel <joelkubinsky@ufl.edu>
2025-01-29 19:52:14 -0500

    Changed things, GPS data is now a 3D plot. The other graphs now a squeezed to much, I'm pulling my hair out. - Joel

--- CSVSimulator.py
+++ CSVSimulator.py
@@ -19,33 +19,31 @@ df.columns = ["TEAM_ID","MISSION_TIME","PACKET_COUNT","MODE","STATE","ALTITUDE",
               "GYRO_R", "GYRO_P", "GYRO_Y", "ACCEL_R", "ACCEL_P", "ACCEL_Y", "MAGN_R", "MAGN_P", "MAGN_Y",
               "AUTO_GRYO_ROTATION_RATE", "GPS_TIME", "GPS_ALTITUDE", "GPS_LATITUDE", "GPS_LONGITUDE", "GPS_SATS", "CMD_ECHO"]
 
-df.loc[0] = [2031, # Team_ID
-            str(datetime.now())[11:][:-7], # Mission_Time
-            packet_count, # Packet_Count
-            mode, # Mode
-            state, # State
-            random.randint(1,100), # Altitude
-            random.randint(1,100), # Temperature
-            random.randint(1, 100), # Pressure
-            random.randint(1, 100), # Voltage
-            random.randint(1,360), # Gyro_R
-            random.randint(1,360), # Gyro_P
-            random.randint(1,360), # Gyro_Y
-            random.randint(1,360), # Accel_R
-            random.randint(1,360), # Accel_P
-            random.randint(1,360), # Accel_Y
-            random.randint(1,100), # Magn_R
-            random.randint(1, 100), # Magn_P
-            random.randint(1, 100), # Magn_Y
-            random.randint(1,10), # Auto_Gyro_Rotation_Rate
-            str(datetime.now())[11:][:-7], # GPS_Time
-            random.randint(1,100), # GPS_Altitude
-            random.randint(1, 100), # GPS_Latitude
-            random.randint(1, 100), # GPS_Longitude
-            random.randint(1, 3), # GPS_Sats
-            cmd] # CMD
-
-packet_count = 2
+df.loc[0] = [3174, # Team_ID                                            1
+            str(datetime.now())[11:][:-7], # Mission_Time               2
+            packet_count, # Packet_Count                                3
+            mode, # Mode                                                4
+            state, # State                                              5
+            random.randint(1,100), # Altitude                     6
+            random.randint(1,100), # Temperature                  7
+            random.randint(1, 100), # Pressure                    8
+            random.randint(1, 100), # Voltage                     9
+            random.randint(1,360), # Gyro_R                       10
+            random.randint(1,360), # Gyro_P                       11
+            random.randint(1,360), # Gyro_Y                       12
+            random.randint(1,360), # Accel_R                      13
+            random.randint(1,360), # Accel_P                      14
+            random.randint(1,360), # Accel_Y                      15
+            random.randint(1,100), # Magn_R                       16
+            random.randint(1, 100), # Magn_P                      17
+            random.randint(1, 100), # Magn_Y                      18
+            random.randint(1,10), # Auto_Gyro_Rotation_Rate       19
+            str(datetime.now())[11:][:-7], # GPS_Time                   20
+            random.randint(1,100), # GPS_Altitude                 21
+            random.randint(1, 100), # GPS_Latitude                22
+            random.randint(1, 100), # GPS_Longitude               23
+            random.randint(1, 3), # GPS_Sats                      24
+            cmd] # CMD                                                  25
 
 
 while(packet_count <= 1000):
@@ -67,7 +65,7 @@ while(packet_count <= 1000):
     packet_count += 1
 
     start = time.perf_counter()
-    df.loc[len(df.index)] = [2031, # Team_ID
+    df.loc[len(df.index)] = [3174, # Team_ID
                             str(datetime.now())[11:][:-7], # Mission_Time
                             packet_count, # Packet_Count
                             mode, # Mode
--- gui.py
+++ gui.py
@@ -2,8 +2,10 @@ import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+from mpl_toolkits.mplot3d import Axes3D
 import random
 import time
+import csv
 
 # Colors and Fonts for Blue and Orange Theme
 PRIMARY_COLOR = '#F0F0F0'   # Light gray
@@ -12,7 +14,14 @@ FONT_TITLE = ("Verdana", 12, "bold")
 CMD_ECHO = ""  # Initialize CMD_ECHO with an empty string
 
 global previous_command
+global curr_packet
+global generate_new_packet
+global packet_counter
+
 previous_command = ""
+curr_packet = ""
+generate_new_packet = True
+packet_counter = 0
 
 # 14 point min text font
 plt.rcParams.update({'font.size': 14})  # Set default font size for plots
@@ -23,7 +32,7 @@ graphs_data = {
     "Temperature": (x_values, [0, 0, 0, 0, 0, 0, 0, 0]),
     "Pressure": (x_values, [0, 0, 0, 0, 0, 0, 0, 0]),
     "GPS_Sats": (x_values, [0, 0, 0, 0, 0, 0, 0, 0]),
-    "GPS_Altitude": (x_values, [0, 0, 0, 0, 0, 0, 0, 0]),
+    # "GPS_Altitude": (x_values, [0, 0, 0, 0, 0, 0, 0, 0]),
     "Accel_R": (x_values, [0, 0, 0, 0, 0, 0, 0, 0]),
     "Accel_P": (x_values, [0, 0, 0, 0, 0, 0, 0, 0]),
     "Accel_Y": (x_values, [0, 0, 0, 0, 0, 0, 0, 0]),
@@ -37,20 +46,76 @@ graphs_data = {
     "Mag_Y": (x_values, [0, 0, 0, 0, 0, 0, 0, 0]),
     "Voltage": (x_values, [0, 0, 0, 0, 0, 0, 0, 0]),
 }
+gyro_latitude_points = [0, 1, 2, 3, 4, 5, 6, 7]
+gyro_longitude_points = [0, 1, 2, 3, 4, 5, 6, 7]
+gyro_altitude_points = [0, 1, 2, 3, 4, 5, 6, 7]
 
-# Function to generate random data for graphs
-def generate_random_data():
+# Collect current graph data from the latest packet  Old: (Function to generate random data for graphs)
+def collect_graph_data():
+    # I'm going to keep the auto scrolling time values as it'll give a good point of reference on whether the packets are stalled
     for i in range(7):
         x_values[i] = x_values[i + 1]
-    x_values[-1] = x_values[-2] + 1
+        gyro_altitude_points[i] = gyro_altitude_points[i + 1]
+        gyro_latitude_points[i] = gyro_latitude_points[i + 1]
+        gyro_longitude_points[i] = gyro_longitude_points[i + 1]
+
+    x_values[7] = curr_packet[0]
+    gyro_altitude_points[7] = int(curr_packet[21])
+    gyro_latitude_points[7] = int(curr_packet[22])
+    gyro_longitude_points[7] = int(curr_packet[23])
+
     for key in graphs_data:
         y_values = graphs_data[key][1]
         for i in range(len(y_values) - 1):
             y_values[i] = y_values[i + 1]
-        if "Temperature" not in key:
-            y_values[-1] = random.randint(0, 100)
-        else:
-            y_values[-1] = random.randint(-20, 40)
+
+        if key == "Altitude":
+            y_values[-1] = int(curr_packet[6])
+
+        elif key == "Temperature":
+            y_values[-1] = int(curr_packet[7])
+
+        elif key == "Pressure":
+            y_values[-1] = int(curr_packet[8])
+
+        elif key == "Voltage":
+            y_values[-1] = int(curr_packet[9])
+
+        elif key in ["Gyro_R", "Gyro_P", "Gyro_Y"]:
+            if key == "Gyro_R":
+                y_values[-1] = int(curr_packet[10])
+            elif key == "Gyro_P":
+                y_values[-1] = int(curr_packet[11])
+            elif key == "Gyro_Y":
+                y_values[-1] = int(curr_packet[12])
+
+        elif key == "Gyro_Rotation_Rate": #"Auto_Gyro_Rotation_Rate":
+            y_values[-1] = int(curr_packet[19])
+
+        elif key in ["Accel_R", "Accel_P", "Accel_Y"]:
+            if key == "Accel_R":
+                y_values[-1] = int(curr_packet[13])
+            elif key == "Accel_P":
+                y_values[-1] = int(curr_packet[14])
+            elif key == "Accel_Y":
+                y_values[-1] = int(curr_packet[15])
+
+        elif key in ["Mag_R", "Mag_P", "Mag_Y"]:
+            if key == "Mag_R":
+                y_values[-1] = int(curr_packet[16])
+            elif key == "Mag_P":
+                y_values[-1] = int(curr_packet[17])
+            elif key == "Mag_Y":
+                y_values[-1] = int(curr_packet[18])
+
+        elif key == "GPS_Altitude":
+            y_values[-1] = int(curr_packet[21])
+
+        elif key == "GPS_Sats":
+            y_values[-1] = int(curr_packet[24])
+
+        # else:
+        #     gyro_graph_data_points[-1] = []
 
         graphs_data[key] = (x_values, y_values)
 
@@ -74,21 +139,18 @@ def plot_all_graphs(fig, axs):
         # Plot data as before
         ax.plot(x, y, marker='o', color='blue', label='X vs Y', linewidth=2.0)
 
-        # Customize y-axis labels and ticks
+        # Customize y and x-axis labels and ticks
         ax.yaxis.set_major_locator(MaxNLocator(nbins=4))
-        
-        # Highlight area under the curve
-        if graph_title == "Temperature":
-            ax.fill_between(x, y, -20, color='orange', alpha=0.2)
-        else:
-            ax.fill_between(x, y, color='orange', alpha=0.2)
+        ax.xaxis.set_major_locator(MaxNLocator(nbins=4))
+
+        # Highlight area under the curve (brokie, no clue why - Joel)
+        # ax.fill_between(x, y, color='orange', alpha=0.2)
 
         # Add title, axis labels, and other formatting
         ax.set_title(graph_title, weight='bold', color='darkblue', fontname='Verdana')
-        ax.set_xlabel('Time (s)', fontname='Verdana')
+        # ax.set_xlabel('Packets', fontname='Verdana')
         ax.set_ylabel('Y Axis', fontname='Verdana')
         ax.grid(True, linestyle='--', alpha=0.6)
-        # ax.legend(loc='upper left')
 
         # Customize y-axis labels based on the graph title
         if graph_title == "Altitude":
@@ -101,7 +163,7 @@ def plot_all_graphs(fig, axs):
             ax.set_ylabel('Voltage (V)', fontname='Verdana')
         elif graph_title in ["Gyro_R", "Gyro_P", "Gyro_Y"]:
             ax.set_ylabel('Gyro Rate (°/s)', fontname='Verdana')
-        elif graph_title == "Gyro_Rotation_Rate": #"Auto_Gyro_Rotation_Rate":
+        elif graph_title == "Gyro_Rotation_Rate":
             ax.set_ylabel('Degrees (°/s)', fontname='Verdana')
         elif graph_title in ["Accel_R", "Accel_P", "Accel_Y"]:
             ax.set_ylabel('Accel (m/s²)', fontname='Verdana')
@@ -112,30 +174,48 @@ def plot_all_graphs(fig, axs):
         elif graph_title == "GPS_Sats":
             ax.set_ylabel('GPS Satellites', fontname='Verdana')
 
-        # plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor") # Rotate time marks
-
-
     # Adjust layout to prevent overlap
-    plt.subplots_adjust(hspace=1.0, wspace=0.4)  # Adjust space between subplots
+    # plt.subplots_adjust(hspace=1.0, wspace=1.4)  # Adjust space between subplots
     canvas.draw()  # Redraw the canvas
 
+def plot_3d_graphs(fig_3d, axs_3d):
+    global gyro_latitude_points, gyro_longitude_points, gyro_altitude_points
+
+    axs_3d.clear()
+    axs_3d.set_title('GPS Position')
+    axs_3d.set_xlabel('GPS Latitude')
+    axs_3d.set_ylabel('GPS Longitude')
+    axs_3d.set_zlabel('GPS Altitude')
+    axs_3d.plot(gyro_latitude_points, gyro_longitude_points, gyro_altitude_points, color='darkblue')
+    fig_3d.patch.set_facecolor('#F0F0F0')
+
+    canvas_3d.draw()
 
 # Function to update the mission time dynamically
 def update_mission_time():
     current_time = time.strftime("Mission Time: %H:%M:%S")
     mission_time_label.config(text=current_time)
-    current_time = time.strftime("Mission (GPS) Time: %H:%M:%S")
-    gps_time_label.config(text=current_time)
+
     root.after(1000, update_mission_time)  # Update every 1 second
 
-# Function to refresh the graphs with new data every second (1 Hz)
+# Function to refresh the graphs with new data every second (1 Hz) (This might change to the "Update Everything" func)
 def update_graphs():
-    global updating_graphs
+    global updating_graphs, curr_packet, packet_counter
+    get_last_csv_row("SimCSV.csv")
+
+    current_time = "GPS Time: " + str(curr_packet[20])
+    gps_time_label.config(text=current_time)
+
+    packet_count_label.config(text=f"Packet Count: {packet_counter}")
+    team_id_label.config(text=f"Team ID: {curr_packet[1]}")
+
     if updating_graphs:
         return  # Skip if an update is already in progress
     updating_graphs = True
-    generate_random_data()  # Generate new random dataf
+    collect_graph_data()  # Generate new random dataf
     plot_all_graphs(fig, axs)  # Update the existing figure and axes
+    plot_3d_graphs(fig_3d, axs_3d)
+
     updating_graphs = False
     root.after(500, update_graphs)  # Schedule the next update in 1 second
 
@@ -155,13 +235,31 @@ def send_command():
         simulation_mode()
 
     # This is because I can't be bothered to look for a nice way to close the window after forced fullscreen - Joel
-    if command == "EXIT":
+    if command in ["EXIT", "exit"]:
         quit()
 
     previous_command = command
     print(f"Previous Command: {previous_command}")
     print(f"Command sent: {command}")  # Placeholder for actual command sending logic
     cmd_entry.delete(0, tk.END)  # Clear the command entry field
+
+def get_last_csv_row(filename):
+    global curr_packet, packet_counter, generate_new_packet
+    if generate_new_packet and not updating_graphs:
+        generate_new_packet = False
+        try:
+            with open(filename, 'r') as file:
+                open_csv = csv.reader(file)
+                last_row = None
+                for row in open_csv:
+                    last_row = row
+                curr_packet = last_row
+                packet_counter += 1
+
+
+        except:
+            curr_packet = [0 for _ in range(26)]
+        generate_new_packet = True
 
 # Create the main window
 root = tk.Tk()
@@ -175,33 +273,25 @@ root.attributes("-fullscreen", True)
 # Initialize updating_graphs flag
 updating_graphs = False
 
-# Static values
-static_values = {
-    "TEAM_ID": "1234",
-    "PACKET_COUNT": "0",
-    "MODE": "IDLE",
-    "STATE": "OK",
-    "GPS_LATITUDE": "N/A",
-    "GPS_LONGITUDE": "N/A",
-}
-
-# Layout for static values
-for idx, (label, value) in enumerate(static_values.items()):
-    # Because the mission time is rendered separately, I have this line to prevent the first columns from being printed over
-    # Adjust the offset, change the increment
-    idx += 1
-    row_index = 0
-    if idx > 6:
-        row_index = 1
-    tk.Label(root, text=f"{label}: {value}", font=FONT_TITLE, bg=PRIMARY_COLOR, fg=TEXT_COLOR).grid(row=row_index, column=idx%7, padx=5, pady=5)
-
 # Create a figure and axes for the plots
-fig, axs = plt.subplots(4, 4, figsize=(20, 25), dpi=120)  # 16 graphs in a 4x4 grid
-fig.patch.set_facecolor('#F0F0F0')  # Light gray background
+fig, axs = plt.subplots(4, 4, figsize=(20, 15), dpi=80)  # 16 graphs in a 4x4 grid
+# fig.patch.set_facecolor('#F0F0F0')  # Light gray background
 canvas = FigureCanvasTkAgg(fig, master=root)
 
+# Create 3D graph
+fig_3d = plt.figure()
+axs_3d = fig_3d.add_subplot(projection='3d')
+axs_3d.set_title('GPS Position')
+axs_3d.set_xlabel('GPS Latitude')
+axs_3d.set_ylabel('GPS Longitude')
+axs_3d.set_zlabel('GPS Altitude')
+axs_3d.plot(gyro_latitude_points, gyro_longitude_points, gyro_altitude_points)
+fig_3d.patch.set_facecolor('#F0F0F0')
+canvas_3d = FigureCanvasTkAgg(fig_3d, master=root)
+
 # Ensure the figure canvas stretches dynamically
-canvas.get_tk_widget().grid(row=3, column=0, columnspan=9, padx=10, pady=10)# , sticky="nsew")
+canvas.get_tk_widget().grid(row=3, column=0, columnspan=4, sticky="nsew")
+canvas_3d.get_tk_widget().grid(row=3, column=9, sticky='nsew')
 
 # Set the main window to resize dynamically
 root.grid_columnconfigure(0, weight=1)
@@ -213,11 +303,11 @@ for col in range(9):  # Total number of columns used in the grid
 root.grid_rowconfigure(0, weight=0)  # Static labels row
 root.grid_rowconfigure(1, weight=0)  # More labels
 root.grid_rowconfigure(2, weight=0)  # CMD entry row
-root.grid_rowconfigure(3, weight=1)  # Graphs row
+root.grid_rowconfigure(3, weight=2)  # Graphs row
 
-# Update CMD label, entry, and button to center them at the bottom
+# Update CMD label, entry, and button to center them above the graphs
 cmd_frame = tk.Frame(root, bg=PRIMARY_COLOR)  # Create a frame to contain the CMD controls
-cmd_frame.grid(row=2, column=0, columnspan=9, padx=10, pady=5, sticky="ew")  # Span the entire width
+cmd_frame.grid(row=2, column=0, columnspan=9,  sticky="ew")  # Span the entire width
 
 cmd_frame.grid_columnconfigure(0, weight=1)  # Left spacer
 cmd_frame.grid_columnconfigure(1, weight=0)  # CMD label
@@ -227,31 +317,55 @@ cmd_frame.grid_columnconfigure(4, weight=1)  # Right spacer
 
 # Create a label for command echo
 cmd_echo_label = tk.Label(cmd_frame, text=f"CMD_ECHO: {CMD_ECHO}", font=FONT_TITLE, bg=PRIMARY_COLOR, fg=TEXT_COLOR)
-cmd_echo_label.grid(row=0, column=0, padx=5, pady=10)
+cmd_echo_label.grid(row=0, column=0, padx=5)
 
 # Add CMD label, entry, and button to the frame
 cmd_label = tk.Label(cmd_frame, text="CMD:", font=FONT_TITLE, bg=PRIMARY_COLOR, fg=TEXT_COLOR)
-cmd_label.grid(row=0, column=1, padx=(0, 5), pady=10, sticky="e")
+cmd_label.grid(row=0, column=1, padx=(0, 5), sticky="e")
 
 cmd_entry = tk.Entry(cmd_frame, font=FONT_TITLE, width=30)
-cmd_entry.grid(row=0, column=2, padx=10, pady=10)
+cmd_entry.grid(row=0, column=2, padx=10)
 
 send_button = tk.Button(cmd_frame, text="Send", font=FONT_TITLE, command=send_command)
-send_button.grid(row=0, column=3, padx=0, pady=10, sticky="w")
+send_button.grid(row=0, column=3, padx=0, sticky="w")
 
 # Create mission time label
 mission_time_label = tk.Label(root, text="--:--:--", font=FONT_TITLE, bg=PRIMARY_COLOR, fg=TEXT_COLOR)
 mission_time_label.grid(row=0, column=0, padx=5, pady=5)
 
-# Create mission time label
-gps_time_label = tk.Label(root, text="--:--:--", font=FONT_TITLE, bg=PRIMARY_COLOR, fg=TEXT_COLOR)
-gps_time_label.grid(row=1, column=0, padx=5, pady=5)
+# Create Team ID label
+team_id_label = tk.Label(root, text="Team ID: 3174", font=FONT_TITLE, bg=PRIMARY_COLOR, fg=TEXT_COLOR)
+team_id_label.grid(row=0, column=1, padx=5, pady=5)
 
-# Start the real-time mission time update
-update_mission_time()
+# Create Packet Count label
+packet_count_label = tk.Label(root, text="Packet Count: 0", font=FONT_TITLE, bg=PRIMARY_COLOR, fg=TEXT_COLOR)
+packet_count_label.grid(row=0, column=2, padx=5, pady=5)
+
+# Create Mode label
+mode_label = tk.Label(root, text="Mode: IDLE", font=FONT_TITLE, bg=PRIMARY_COLOR, fg=TEXT_COLOR)
+mode_label.grid(row=0, column=3, padx=5, pady=5)
+
+# Create State label
+state_label = tk.Label(root, text="State: OK", font=FONT_TITLE, bg=PRIMARY_COLOR, fg=TEXT_COLOR)
+state_label.grid(row=1, column=1, padx=5, pady=5)
+
+# # Create GPS Latitude label
+# gps_latitude_label = tk.Label(root, text="GPS Latitude: N/A", font=FONT_TITLE, bg=PRIMARY_COLOR, fg=TEXT_COLOR)
+# gps_latitude_label.grid(row=0, column=5, padx=5, pady=5)
+#
+# # Create GPS Longitude label
+# gps_longitude_label = tk.Label(root, text="GPS Longitude: N/A", font=FONT_TITLE, bg=PRIMARY_COLOR, fg=TEXT_COLOR)
+# gps_longitude_label.grid(row=0, column=6, padx=5, pady=5)
+
+# Create GPS time label
+gps_time_label = tk.Label(root, text="--:--:--", font=FONT_TITLE, bg=PRIMARY_COLOR, fg=TEXT_COLOR)
+gps_time_label.grid(row=1, column=0, padx=5)
 
 # Start updating graphs every second
 update_graphs()
+
+# Start the real-time mission time update
+update_mission_time()
 
 # Start the Tkinter event loop
 root.mainloop()
